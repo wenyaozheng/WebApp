ec761379297ada6b1044f139a65773be6dee3f9c
Charlene T. Fama <charlenefama18@gmail.com>
2017-03-30 01:28:54 +0800

    added classcode and student list functionality

---
+++ web-app/js/show_list.js
@@ -0,0 +1,79 @@
+function CreateTableFromJSON(data) {
+    var input = data;
+   if(input == "class_9413A") {
+      var studList = [
+           {
+               "ID No.": "2150428",
+               "Last Name": "Fama",
+               "First Name": "Charlene"
+           },
+           {
+               "ID No.": "2150000",
+               "Last Name": "Garcia",
+               "First Name": "Jay"
+           },
+           {
+               "ID No.": "2150001",
+               "Last Name": "Zheng",
+               "First Name": "Anne"
+           }
+       ];
+   } else if (input == "class_9415B") {
+       var studList = [
+           {
+               "ID No.": "SAMPLE",
+               "Last Name": "...",
+               "First Name": "..."
+           },
+           {
+               "ID No.": "2150000",
+               "Last Name": "...",
+               "First Name": "..."
+           },
+           {
+               "ID No.": "...",
+               "Last Name": "...",
+               "First Name": "..."
+           }
+       ];
+   }
+
+    // EXTRACT VALUE FOR HTML HEADER.
+    var col = [];
+    for (var i = 0; i < studList.length; i++) {
+        for (var key in studList[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+
+    // CREATE DYNAMIC TABLE.
+    var table = document.createElement("table");
+
+    // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement("th");      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+
+    // ADD JSON DATA TO THE TABLE AS ROWS.
+    for (var i = 0; i < studList.length; i++) {
+
+        tr = table.insertRow(-1);
+
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = studList[i][col[j]];
+        }
+    }
+
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("showStudentList");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
+}
