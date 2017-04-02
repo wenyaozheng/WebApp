ab9aeac1c62cf9ff7b1fde1be1ce11a0ec20b832
Jay Garcia <2151796@slu.edu.ph>
2017-04-02 21:32:08 +0800

    edited classcode buttons

--- web-app/js/classcodes.js
+++ web-app/js/classcodes.js
@@ -1,12 +1,18 @@
 /**
  * Created by s326lab on 31/03/2017.
  */
-var text, obj, i, output = ""
+var text, obj, i, counter=0,classcode;
 text = localStorage.getItem("testJSON");
 obj = JSON.parse(text);
  for(i in obj.Classrooms) {
-    output = obj.Classrooms[i].classcode + " " + obj.Classrooms[i].subject;
-     document.write( '<li id="blue">'+output+'</li>');
+    output = obj.Classrooms[i].classcode + " " + obj.Classrooms[i].subject+" "+obj.Classrooms[i].schedule;
+    if(counter<=4) {
+        counter = counter + 1;
+    }
+    else{
+        counter=1;
+    }
+     document.write( '<li id="color'+counter+'" onclick="">'+output+'</li>');
  }
 // console.log(output);
 //document.getElementById("blue").innerHTML = output;
