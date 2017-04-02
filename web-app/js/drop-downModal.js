33d696c16be1772d62e0e63333350cc45f13d52f
Jay Garcia <2151796@slu.edu.ph>
2017-04-02 22:46:10 +0800

    fixed dropdown modal

--- web-app/js/drop-downModal.js
+++ web-app/js/drop-downModal.js
@@ -6,33 +6,91 @@ var text, obj, i;
 text = localStorage.getItem("listJSON");
 obj = JSON.parse(text);
 var input = "";
-input = localStorage.getItem("classcode");
-
-function dropDown() {
-
+var write = "";
+ // input = localStorage.getItem("classcode");
+ // dropDown(input);
+function dropDown(input) {
+    console.log(input);
     switch(input) {
         case "class_9413A":
+            var myNode = document.getElementById("selector");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+            var option = document.createElement("option");
+            option.text = "";
+            option.value = "";
+            var select = document.getElementById("selector");
+            select.appendChild(option);
             for(i in obj.class_9413A) {
                 output = obj.class_9413A[i].LastName + " " + obj.class_9413A[i].FirstName;
-                document.write( '<option value="'+obj.class_9413A[i].IDNo+'">'+output+'</option>');
+                var option = document.createElement("option");
+                option.text = output;
+                option.value = ""+obj.class_9413A[i].IDNo;
+                var select = document.getElementById("selector");
+                select.appendChild(option);
+                // document.write( '<option value="'+obj.class_9413A[i].IDNo+'">'+output+'</option>');
+
             }
             break;
         case "class_9413B":
+            var myNode = document.getElementById("selector");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+            var option = document.createElement("option");
+            option.text = "";
+            option.value = "";
+            var select = document.getElementById("selector");
+            select.appendChild(option);
             for(i in obj.class_9413B) {
                 output = obj.class_9413B[i].LastName + " " + obj.class_9413B[i].FirstName;
-                document.write( '<option value="'+obj.class_9413B[i].IDNo+'">'+output+'</option>');
+                var option = document.createElement("option");
+                option.text = output;
+                option.value = ""+obj.class_9413B[i].IDNo;
+                var select = document.getElementById("selector");
+                select.appendChild(option);
+                // document.write('<option value="'+obj.class_9413B[i].IDNo+'">'+output+'</option>');
             }
             break;
         case "class_9414A":
+            var myNode = document.getElementById("selector");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+            var option = document.createElement("option");
+            option.text = "";
+            option.value = "";
+            var select = document.getElementById("selector");
+            select.appendChild(option);
             for(i in obj.class_9414A) {
                 output = obj.class_9414A[i].LastName + " " + obj.class_9414A[i].FirstName;
-                document.write( '<option value="'+obj.class_9414A[i].IDNo+'">'+output+'</option>');
+                var option = document.createElement("option");
+                option.text = output;
+                option.value = ""+obj.class_9414A[i].IDNo;
+                var select = document.getElementById("selector");
+                select.appendChild(option);
+                // document.write('<option value="'+obj.class_9414A[i].IDNo+'">'+output+'</option>');
             }
             break;
         case "class_9414B":
+            var myNode = document.getElementById("selector");
+            while (myNode.firstChild) {
+                myNode.removeChild(myNode.firstChild);
+            }
+            var option = document.createElement("option");
+            option.text = "";
+            option.value = "";
+            var select = document.getElementById("selector");
+            select.appendChild(option);
             for(i in obj.class_9414B) {
                 output = obj.class_9414B[i].LastName + " " + obj.class_9414B[i].FirstName;
-                document.write( '<option value="'+obj.class_9414B[i].IDNo+'">'+output+'</option>');
+                var option = document.createElement("option");
+                option.text = output;
+                option.value = ""+obj.class_9414B[i].IDNo;
+                var select = document.getElementById("selector");
+                select.appendChild(option);
+                // document.write('<option value="'+obj.class_9414B[i].IDNo+'">'+output+'</option>');
             }
             break;
         default:
