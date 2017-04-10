e9e0ea85352e629d49b23415d1150c520d99e3d6
Charlene Fama <charlenefama18@gmail.com>
2017-04-10 23:58:27 +0800

    added seatPlan for lab classes

--- web-app/js/studentCanvas.js
+++ web-app/js/studentCanvas.js
@@ -99,6 +99,50 @@ function DefinePaths(event){
             temp=temp+1;
         }
     }
+    // seatPlan_lab
+    // var temp=1;
+    // var asd;
+    // var column, row;
+    // for (var x = 5, i = 0; i < column; x += 25, i++) {
+    //     for (var y = 5, j = 0; j < row; y += 25, j++) {
+    //         ctx.beginPath();
+    //         ctx.rect(x, y, 20, 20);
+    //         if (event!=null){
+    //             if (IsInPath(event)) {
+    //                 set=temp;
+    //                 for(asd in obj.class_9413A) {
+    //                     if(obj.class_9413A[i].SeatNo==temp){
+    //                         SelStyle2();
+    //                     }
+    //                     else{
+    //                         SelStyle();
+    //                     }
+    //                 }
+    //             }
+    //             else
+    //                 DifStyle();
+    //         }else
+    //             DifStyle();
+    //         for(asd in obj.class_9413A) {
+    //             if(obj.class_9413A[asd].SeatNo==temp){
+    //                 // console.log(obj.class_9413A[i].LastName+ " " + obj.class_9413A[i].FirstName);
+    //                 // console.log( obj.class_9413A[i].SeatNo);
+    //                 boxcolor.push(temp);
+    //             }
+    //         }
+    //         for(asd in boxcolor){
+    //             if(boxcolor[asd]==temp){
+    //                 console.log(boxcolor[asd]);
+    //                 SelStyle1();
+    //             }
+    //         }
+    //         ctx.closePath();
+    //         ctx.fill();
+    //         ctx.strokeText(temp+"",x+6,y+13);
+    //         ctx.stroke();
+    //         temp=temp+1;
+    //     }
+    // }
 }
 
 function IsInPath(event) {
@@ -171,7 +215,6 @@ function setName(){
                     localStorage.removeItem("classListJSON");
 
                     localStorage.setItem("classListJSON",JSON.stringify(obj));
-
                 }
             }
             break;
@@ -262,6 +305,58 @@ function printStudentList(input){
             break;
         default:
             // console.log("error");
+    }
+}
+function printStudentList(input){
+    // console.log(input);
+    var i, ul, li, myNode;
+    myNode = document.getElementById("generateList");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+    li = document.createElement("li");
+    li.text = "";
+    var select = document.getElementById("generateList");
+    select.appendChild(li);
+    switch(input) {
+        case "class_9413A":
+            for (i in obj.class_9413A) {
+                ul = document.getElementById("generateList");
+                li = document.createElement("li");
+                output = obj.class_9413A[i].SeatNo + ' ' + obj.class_9413A[i].LastName + ', ' + obj.class_9413A[i].FirstName;
+                li.appendChild(document.createTextNode(output));
+                ul.appendChild(li);
+            }
+            break;
+        case "class_9413B":
+            for (i in obj.class_9413B) {
+                ul = document.getElementById("generateList");
+                li = document.createElement("li");
+                output = obj.class_9413B[i].SeatNo + ' ' + obj.class_9413B[i].LastName + ', ' + obj.class_9413B[i].FirstName;
+                li.appendChild(document.createTextNode(output));
+                ul.appendChild(li);
+            }
+            break;
+        case "class_9414A":
+            for (i in obj.class_9414A) {
+                ul = document.getElementById("generateList");
+                li = document.createElement("li");
+                output = obj.class_9414A[i].SeatNo + ' ' + obj.class_9414A[i].LastName + ', ' + obj.class_9414A[i].FirstName;
+                li.appendChild(document.createTextNode(output));
+                ul.appendChild(li);
+            }
+            break;
+        case "class_9414B":
+            for (i in obj.class_9414B) {
+                ul = document.getElementById("generateList");
+                li = document.createElement("li");
+                output = obj.class_9414B[i].SeatNo + ' ' + obj.class_9414B[i].LastName + ', ' + obj.class_9414B[i].FirstName;
+                li.appendChild(document.createTextNode(output));
+                ul.appendChild(li);
+            }
+            break;
+        default:
+        // console.log("error");
     }
 }
 
