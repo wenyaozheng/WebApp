0c643b9de42e159e0713b83c57276fbf18e1ce7e
Charlene Fama <charlenefama18@gmail.com>
2017-04-15 23:57:42 +0800

    fixed link functions issue

--- web-app/js/classcodes.js
+++ web-app/js/classcodes.js
@@ -4,25 +4,30 @@
 var text, obj, i, counter=0,classcode;
 text = localStorage.getItem("classroomListJSON");
 obj = JSON.parse(text);
+var j = 0;
 for(i in obj.Classrooms) {
+
     output = obj.Classrooms[i].classcode + " " + obj.Classrooms[i].subject+" "+obj.Classrooms[i].schedule;
-    classcode = obj.Classrooms[i].classcode;
     if(counter<=4) {
         counter = counter + 1;
     }
     else{
         counter=1;
     }
-    document.write( '<li id="color'+counter+'" onclick="">'+output+'</li>');
+    // document.write( '<li id="color'+counter+'" onclick="">'+output+'</li>');
 
-    var ul = document.getElementById("classList");
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(output));
-    li.setAttribute("id", "color"+counter);
+    if(i == j){
+        var ul = document.getElementById("classList");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(output));
+        li.setAttribute("id", "color"+counter);
+        classcode = obj.Classrooms[i].classcode;
+        var att = document.createAttribute("onclick");
+        att.value = "temp_classcode('class_" + classcode + "'); " + + "printStudentList('class_" +classcode+"');" + "clearBox(); " + "DefinePaths(event);";
+        li.setAttributeNode(att);
 
-    var att = document.createAttribute("onclick");
-    att.value = temp_classcode(classcode);
-    li.setAttributeNode(att);
+        ul.appendChild(li);
+        j = j+1;
+    }
 
-    ul.appendChild(li);
 }
--- web-app/json/temp.js
+++ web-app/json/temp.js
@@ -2,12 +2,9 @@
  * Created by Ling on 4/2/2017.
  */
 function temp_classcode(input){
-    window.localStorage.removeItem("classcode");
-    localStorage.setItem("classcode", input);
     console.log(input);
     colorScheme(input);
     dropDown(input);
-    printStudentList(input);
 }
 
 function storeData(){
